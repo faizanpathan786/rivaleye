b0742bdb0f016ad835b00294c3be7275b163c7e3
Ayman Parkar <ayman.parkar@gmail.com>
2026-05-16 02:52:05 +0530

    feat: add worker drizzle client

---
+++ packages/worker/src/db.ts
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "../../api/src/db/schema/index.js";
+
+const connectionString = process.env.CONNECTION_STRING;
+if (!connectionString) throw new Error("CONNECTION_STRING is required");
+
+const queryClient = postgres(connectionString);
+export const db = drizzle(queryClient, { schema });
